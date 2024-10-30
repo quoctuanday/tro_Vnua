d597c0c64f9b39a38977e125d5c6b717995686e9
quoctuanday <nguyenquoctuan0061@gmail.com>
2024-10-30 18:01:44 +0700

    first commmit

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const { PORT, CLIENT_PORT } = require('./config/env');
+const db = require('./config/db');
+const route = require('./routes');
+
+const app = express();
+db.connect();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+route(app);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
